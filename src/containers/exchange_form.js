3f27f99dab50d1e0122da3643b60b47eadabd03a
mpacek <m.pacek@onet.eu>
2017-09-20 11:49:18 +0200

    Amount sign fix

--- src/containers/exchange_form.js
+++ src/containers/exchange_form.js
@@ -38,7 +38,7 @@ class ExchangeForm extends Component {
     if (!!this.props.exchange.amount !== false) {
       exchangeAmount =
         <div className="exchange-form__input exchange-form__input--to">
-          {this.props.exchange.amount > 0 && '+'}{_.round(this.props.exchange.amount * this.props.exchange.rate, 2)}
+          {this.props.exchange.amount < 0 && '+'}{-1 * _.round(this.props.exchange.amount * this.props.exchange.rate, 2)}
         </div>;
     }
 
